dfd0b16bb41583d2c8e8f608b4e35693d4f384bc
guruadp <gurunandhanadp@gmail.com>
2022-01-31 08:44:09 +0530

    added launch file

---
+++ 2 Wheeled Robot/simulation_ws/build/m2wr_descripption/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/guruadp/New Volume/Projects/ROS/2 Wheeled Robot/simulation_ws/src/m2wr_descripption
+# Build directory: /media/guruadp/New Volume/Projects/ROS/2 Wheeled Robot/simulation_ws/build/m2wr_descripption
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ 2 Wheeled Robot/simulation_ws/build/m2wr_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/guruadp/New Volume/Projects/ROS/2 Wheeled Robot/simulation_ws/src/m2wr_description
+# Build directory: /media/guruadp/New Volume/Projects/ROS/2 Wheeled Robot/simulation_ws/build/m2wr_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
