c8194aaf7611771f35508a8042eba91853a9ee35
guruadp <gurunandhanadp@gmail.com>
2022-02-02 10:13:35 +0530

    Created ROS basics project

---
+++ ROS_basics/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/media/guruadp/New Volume/Projects/ROS/Construct_ROS_basics/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/media/guruadp/New Volume/Projects/ROS/2 Wheeled Robot/simulation_ws/devel;/opt/ros/noetic'.split(';') if '/media/guruadp/New Volume/Projects/ROS/2 Wheeled Robot/simulation_ws/devel;/opt/ros/noetic' != '' else []
